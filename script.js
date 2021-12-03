278fedc12f6ffe42a692641db63d6f75f167a766
MorcaDev <morca225@gmail.com>
2021-12-03 11:56:14 -0500

    hamburger menu mobile - done (1st feature)

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log(`Hi there`)
+
+let navMenu      = document.getElementById("nav-sections");
+let navLeft     = document.getElementById("nav-menu_brand");
+let navRight    = document.getElementById("nav-cart_user");
+
+let containerMain   = document.querySelector("main");
+let containerFooter = document.querySelector("footer");
+
+let openMenu    = document.getElementById("burger-open");
+let closeMenu   = document.getElementById("burger-close");
+
+let burgerMenu = (ev) =>{
+
+    let cssShowMenu = "show-nav-sections";
+    navMenu.classList.toggle(cssShowMenu)
+
+    let cssOpacity = "opacity-containers"; 
+    navLeft.classList.toggle(cssOpacity)
+    navRight.classList.toggle(cssOpacity)
+    containerMain.classList.toggle(cssOpacity)
+    containerFooter.classList.toggle(cssOpacity)
+    
+}
+
+openMenu.addEventListener("click",burgerMenu)
+closeMenu.addEventListener("click",burgerMenu)
